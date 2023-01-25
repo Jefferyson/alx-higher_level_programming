be833790e5a8576391ad9bcd6986b729d00b640b
jefferyson <gokikiola007@gmail.com>
2023-01-25 04:20:51 -0800

    0

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module 1-square
+Define class Square with private attribute size
+"""
+
+
+class square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """
+        Initializes square
+
+        Attributes:
+            size: size of a square
+        """
+        self.__size = size
