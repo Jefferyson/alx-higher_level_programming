0944339b33b38e314c906c150e1d4599011aa03c
jefferyson <gokikiola007@gmail.com>
2023-02-06 15:51:47 -0800

    0

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a class-checking function."""
+
+
+def is_same_class(obj, a class):
+    """return true if obj is exact class a_class, otherwise, false"""
+    return type(obj) == a_class)
