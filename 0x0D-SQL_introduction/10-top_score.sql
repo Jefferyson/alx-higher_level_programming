fee9094fe5fdfc5b5a9d0734dad34fd0a396c0e6
jefferyson <gokikiola007@gmail.com>
2023-03-15 06:30:50 -0700

    0

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,5 @@
---Script that lists all records of the table second_table 
+--Script that lists all records of the table second_table
+--Records are 0rdered by descending score
 SELECT `score`, `name`
-FROM `second_table` 
+FROM `second_table`
 ORDER BY `score` DESC;
