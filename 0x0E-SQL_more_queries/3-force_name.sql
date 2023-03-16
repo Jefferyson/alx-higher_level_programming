e81d7733cd83eb4d4bc68c4abea5595cfc893adb
jefferyson <gokikiola007@gmail.com>
2023-03-16 13:48:01 -0700

    0

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,6 @@
--- Script that creates table
+-- Creates a table with a constraint on a column
+-- to not be null
 CREATE TABLE IF NOT EXISTS force_name(
-	id INT
-	name VARCHAR(256) NOT NULL
+    id INT,
+    name VARCHAR(256) NOT NULL
 );
