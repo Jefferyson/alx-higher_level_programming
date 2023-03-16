4458c7ec11bf24b6ea766d7cbdfd4222b2474c49
jefferyson <gokikiola007@gmail.com>
2023-03-16 13:51:51 -0700

    0

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,4 +1,5 @@
--- Script that creates table unique_id 
+-- Creates a table with a constraint on a column to have a unique value
+-- for each row, which is never empty when a value isn't given 
 CREATE TABLE IF NOT EXISTS `unique_id (
 	`id` INT DEFAULT 1 UNIQUE,
 	`name` VARCHAR(256)
