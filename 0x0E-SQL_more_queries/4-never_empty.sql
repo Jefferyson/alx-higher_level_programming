2a6c4ee739822b470bed05add9ab57506550f25e
jefferyson <gokikiola007@gmail.com>
2023-03-16 13:49:45 -0700

    0

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,6 @@
--- Script that creates the table id_not_null 
+-- Creates a table with a constraint on a column and another
+-- column that is never empty when given a NULL value 
 CREATE TABLE IF NOT EXISTS `id_not_null` (
-	`id` INT DEFAULT 1
+	`id` INT DEFAULT 1,
 	`name` VARCHAR(256) 
 	);
