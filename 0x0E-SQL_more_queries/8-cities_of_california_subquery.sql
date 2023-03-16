c2fc81da7fc6d4f65e64d59b0805c3a5fdb4b6e0
jefferyson <gokikiola007@gmail.com>
2023-03-16 14:04:44 -0700

    0

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,10 @@
--- Script that lists all the cities that can be found in the database
-SELECT `id`, `name`
-FROM `cities`
-WHERE `state_id` IN
-	(SELECT `id`,
-		FROM `states`,
-		WHERE `name` = "Califirnia")
-	ORDER BY `id`ASC;
+-- Selects records from one table (cities) with a
+-- column value that can be found in another set
+SELECT id, name FROM cities
+    WHERE state_id IN
+    (
+        SELECT id FROM states
+            WHERE name = 'California'
+    )
+    ORDER BY id ASC;
+
