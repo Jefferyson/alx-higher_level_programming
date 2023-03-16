c73860d5a340a652b8c9eb646b0bdbfc665d9d9e
jefferyson <gokikiola007@gmail.com>
2023-03-16 14:16:17 -0700

    0

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,4 @@
--- script that lists all genres from database and displays the number of shows linked together
+-- Prints and joins records from two tables with a matching field
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres
 RIGHT JOIN tv_show_genres
